a8b609a06c0e7467f105a968698a96ac22e7d708
Vlad <vladfoton86@gmail.com>
2022-03-22 14:27:34 +0300

    task_2

--- lesson_5/task_2/task_1.go
+++ lesson_5/task_2/task_1.go
@@ -1,1 +1,34 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+	entersValues()
+}
+
+var number int
+var fibonacciNumbersMap = map[int]int{
+	0: 0,
+	1: 1}
+
+func countFibbonachi(number int) int {
+	return ((number - 1) + (number - 2))
+}
+
+func entersValues() {
+	fmt.Print("Введите порядковый номер числа Фибоначи начиная с 0 позиции: ")
+	fmt.Scanln(&number)
+	checkFibonacci(number)
+}
+
+func checkFibonacci(number int) {
+	if v, found := fibonacciNumbersMap[number]; found {
+		fmt.Println(fibonacciNumbersMap[v])
+	} else {
+		fibonacciNumbersMap[number] = countFibbonachi(number)
+		fmt.Println(countFibbonachi(number))
+	}
+	entersValues()
+}
